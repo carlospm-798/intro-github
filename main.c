ea6dc3d6513783760122f0124effc57c8e95df22
Carlos Paredes <carlos.paredes6900@alumnos.udg.mx>
2020-09-16 14:43:18 -0500

    Agregamos <Nombre>, e imprimimos <Nombre>.

--- main.c
+++ main.c
@@ -2,7 +2,14 @@
 
 int main(void){
 
+    char nombre[100];
+
     printf("Hola mundo :D\n");
+    
+    printf("Nombre: ");
+    scanf("%s",&nombre);
+
+    printf("\nHola \t<<%s>>\n",nombre);
 
     return 0;
 }
