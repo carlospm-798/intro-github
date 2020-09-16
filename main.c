1a8a24a022f9a59427be8b5b00469540b22191f9
Carlos Paredes <carlos.paredes6900@alumnos.udg.mx>
2020-09-16 15:06:29 -0500

    Agregamos edad, e imprimimos edad + 1

--- main.c
+++ main.c
@@ -3,13 +3,17 @@
 int main(void){
 
     char nombre[100];
+    unsigned int edad;
 
     printf("Hola mundo :D\n");
     
-    printf("Nombre: ");
-    scanf("%s",&nombre);
+    printf("Nombre:    ");
+    fgets(nombre, sizeof(nombre), stdin);
+    printf("\nHola \t<<%s>>\n", nombre);
 
-    printf("\nHola \t<<%s>>\n",nombre);
+    printf("Que edad tienes?    ");
+    scanf("%u",&edad);
+    printf("\nVas a cumplir \t<%u>\t", edad + 1);
 
     return 0;
 }
