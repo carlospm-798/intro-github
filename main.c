372d7b512574112b90efc2d7522172f44ff750c8
Carlos Paredes <carlos.paredes6900@alumnos.udg.mx>
2020-09-16 14:30:41 -0500

    Funciona el Hola mundo :D

--- main.c
+++ main.c
@@ -2,16 +2,7 @@
 
 int main(void){
 
-    char nombre[100];
-    int year=0;
-
-    printf("Hola Mundo.\n");
-    printf("Inserte su nombre: \n");
-    fgets(nombre, sizeof(nombre),stdin);
-    printf("Hola <<%s>>\n",nombre);
-    printf("Cuantos años tienes actualmente <<%s>>?\n");
-    scanf("%i",&year);
-    printf("<<%s>>, vas a cumplir [%i] años\n",nombre,year);
+    printf("Hola mundo :D\n");
 
     return 0;
 }
