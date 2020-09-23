f98214224df1135d74588d90a5a1aef17fa8c5c4
Carlos Paredes <carlos.paredes6900alumnos.udg.mx>
2020-09-22 19:00:16 -0500

    Comentamos el Hola Mundo :D

--- main.c
+++ main.c
@@ -5,7 +5,7 @@ int main(void){
     char nombre[100];
     unsigned int edad;
 
-    printf("Hola mundo :D\n");
+    /*printf("Hola mundo :D\n");*/
     
     printf("Nombre:    ");
     fgets(nombre, sizeof(nombre), stdin);
